b193a5f75201623142e1c274d0766d0c6b88168b
Holly Moody <hollymoody@charter.net>
2014-11-10 16:33:03 -0500

    Fix users controller based on code review.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,11 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    render json: @users
   end
 
   def show
     @user = User.find(params[:id])
   end
-
-def new
-end
 
  def create
     @user = User.new(user_params)
