112a8ee988254e567dc08b7908ce60e5dbdec393
raielin@gmail.com <raielin@gmail.com>
2014-11-10 17:01:54 -0500

    Added basic index, show, and create actions in menus controller

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,19 @@
 class MenusController < ActionController::API
+
+  def index
+    @menus = Menu.all
+  end
+
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
+  def create
+    @menu = Menu.create(menu_params)
+  end
+
+  private
+  def menu_params
+    params.require(:menu).permit(:name)
+  end
 end
