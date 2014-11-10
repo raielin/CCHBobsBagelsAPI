e5b9e4da52069eecde2f8b169bf8c1a3d24b8f61
raielin@gmail.com <raielin@gmail.com>
2014-11-10 17:05:27 -0500

    Added basic index, show, and create actions in menu items controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,2 +1,18 @@
 class MenuItemsController < ActionController::API
+  def index
+    @menu_items = MenuItem.all
+  end
+
+  def show
+    @menu_item = MenuItem.find(params[:id])
+  end
+
+  def create
+    @menu_item = MenuItem.create(menu_item_params)
+  end
+
+  private
+  def menu_item_params
+    params.require(:menu_item).permit(:name, :price, :menu_id)
+  end
 end
