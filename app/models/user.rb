7261b00630206e80d7d504ff9b1e5792b671bdbb
cyurtbil <cenky92@gmail.com>
2014-11-10 16:36:26 -0500

    commented out validations for simple understanding of user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   validates :email, presence: true
-  validates :password_digest, presence: true
-  validates :access_token, presence: true
-  validates :token_scope, presence: true
-  validates :token_expired_at, presence: true
-  validates :token_created_at, presence: true
+  # validates :password_digest
+  # validates :access_token
+  # validates :token_scope
+  # validates :token_expired_at
+  # validates :token_created_at
 # Need to add validations for uniqueness
 end
