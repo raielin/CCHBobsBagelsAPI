1f5ea100e6cbb3f56023ebd32d6cfc95d2bdcb61
raielin@gmail.com <raielin@gmail.com>
2014-11-10 22:42:55 -0500

    Added comment for inclusion of number helper for further explanation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
    include ActionController::ImplicitRender
+   # Gives us the number_to_currency method, i.e. number_to_currency(MenuItem.find_by_name("Latte").price)
    include ActionView::Helpers::NumberHelper
 end
