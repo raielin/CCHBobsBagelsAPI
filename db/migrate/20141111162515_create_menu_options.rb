8d8522b2efc2db03d0dea8c22c5718eb7cd869f9
raielin@gmail.com <raielin@gmail.com>
2014-11-11 11:44:52 -0500

    Renamed migration table prior to running migration

--- db/migrate/20141111162515_create_menu_options.rb
+++ db/migrate/20141111162515_create_menu_options.rb
@@ -1,6 +1,6 @@
-class CreateAllMenuOptions < ActiveRecord::Migration
+class CreateMenuOptions < ActiveRecord::Migration
   def change
-    create_table :all_menu_options do |t|
+    create_table :menu_options do |t|
       t.belongs_to :menu_options_category, index: true
       t.string :name
       t.decimal :price, precision: 5, scale: 2
