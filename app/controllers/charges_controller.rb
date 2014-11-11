ae82f22e4b668c817a8b1dad3540b79f887a533d
cyurtbil <cenky92@gmail.com>
2014-11-10 20:19:51 -0500

    added charge routes, add create and new methods to charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,3 +1,9 @@
 class ChargesController < ApplicationController
 
+  def new
+  end
+
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :users
+  resources :charges
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
