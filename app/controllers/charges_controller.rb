0c9a5920121dc9158c1b51cc1cfdc5ad4c87aadd
cyurtbil <cenky92@gmail.com>
2014-11-10 20:32:03 -0500

    now create method can create customer and charge them, configured my api keys in stripe initializer

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,6 +4,26 @@ class ChargesController < ApplicationController
   end
 
   def create
+
+    @amount = 10
+
+    customer = Stripe::Customer.create(
+      email: 'example@stripe.com',
+      card: params[:access_token]
+    )
+
+    save_stripe_customer_id(user, customer.id)
+
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: @amount,
+      description: 'Rails Stripe customer',
+      currency: 'usd'
+    )
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to charges_path
   end
 
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,6 @@
-require "stripe"
+Rails.configuration.stripe = {
+  :publishable_key => ENV['STRIPE_SECRET_TEST'],
+  :secret_key      => ENV['STRIPE_PUB_KEY']
+}
 
-if Rails.env == 'development' || Rails.env == 'staging'
-  Stripe.api_key = ENV["STRIPE_SECRET_TEST"]
-  STRIPE_PUB_KEY = "pk_test_0fbtu0To5Q8TurGcFy6XZ505"
-elsif Rails.env == 'production'
-  Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-  STRIPE_PUB_KEY = "pk_live_IoRjUJe9XLoURIzzL6KgURw2"
-end
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
