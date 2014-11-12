8de76deca3c1ebfa769cb45c4251c7815860cbf0
cyurtbil <cenky92@gmail.com>
2014-11-12 16:52:01 -0500

    change the configuration status

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,6 +4,6 @@ if Rails.env == 'development' || Rails.env == 'staging'
   Stripe.api_key = ENV["STRIPE_SECRET_TEST"]
   STRIPE_PUB_KEY = "pk_test_0fbtu0To5Q8TurGcFy6XZ505"
 elsif Rails.env == 'production'
-  Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
-  STRIPE_PUB_KEY = "pk_live_IoRjUJe9XLoURIzzL6KgURw2"
+  Stripe.api_key = ENV["STRIPE_SECRET_TEST"]
+  STRIPE_PUB_KEY = "pk_test_0fbtu0To5Q8TurGcFy6XZ505"
 end
